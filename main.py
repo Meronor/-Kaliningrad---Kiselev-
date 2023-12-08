ee72f50ff411900bb76e827755f39f7cd78e627e
Borya <B.kisselev@gmail.com>
2023-12-08 23:18:34 +0200

    first

---
+++ main.py
@@ -0,0 +1,91 @@
+import sys
+from math import sin, cos, pi
+from random import randint
+
+from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication
+import io
+from PyQt5 import uic
+
+template = '''<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>MainWindow</class>
+ <widget class="QMainWindow" name="MainWindow">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>800</width>
+    <height>600</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>MainWindow</string>
+  </property>
+  <widget class="QWidget" name="centralwidget">
+   <widget class="QPushButton" name="btn">
+    <property name="geometry">
+     <rect>
+      <x>340</x>
+      <y>240</y>
+      <width>75</width>
+      <height>23</height>
+     </rect>
+    </property>
+    <property name="text">
+     <string>Click me</string>
+    </property>
+   </widget>
+  </widget>
+  <widget class="QMenuBar" name="menubar">
+   <property name="geometry">
+    <rect>
+     <x>0</x>
+     <y>0</y>
+     <width>800</width>
+     <height>21</height>
+    </rect>
+   </property>
+  </widget>
+  <widget class="QStatusBar" name="statusbar"/>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>
+'''
+
+
+class Suprematism(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        f = io.StringIO(template)
+        uic.loadUi(f, self)
+        self.flag = False
+        self.setGeometry(300, 300, 1000, 1000)
+        self.qp = QPainter()
+        self.btn.clicked.connect(self.drawf)
+
+    def drawf(self):
+        self.flag = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.flag:
+            self.qp = QPainter()
+            self.qp.begin(self)
+            self.draw()
+            self.qp.end()
+
+    def draw(self):
+        R = randint(20, 100)
+        self.qp.setBrush(QColor('#ffd42e'))
+        self.qp.drawEllipse(int(randint(0, 1000) - R / 2),
+                            int(randint(0, 1000) - R / 2), R, R)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Suprematism()
+    ex.show()
+    sys.exit(app.exec_())
